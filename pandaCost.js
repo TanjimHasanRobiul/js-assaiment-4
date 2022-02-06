4924b09b7c1cddaaf441ee6e284af6d9ab8ca98d
Tanjimhasanrobiul <dvillage.pavelab@gmail.com>
2022-02-06 21:01:10 +0600

    food hisab

---
+++ pandaCost.js
@@ -0,0 +1,14 @@
+const foods = [
+    {name:'singara' , price:7, quantity:5 },
+    {name:'samucha' , price:10, quantity:2 },
+    {name:'jilapi' , price:15, quantity:3 }
+];
+
+let foodstotal = 0;
+
+for (const food of foods){
+    console.log(food);
+    const foodtotal = food.price*food.quantity;
+    foodstotal = foodstotal + foodtotal;
+    } 
+console.log(foodstotal);
